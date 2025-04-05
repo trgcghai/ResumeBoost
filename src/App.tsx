8f1037887a1665c4edf66ce88e19dbadc2bfe41a
trgcghai <conghai.tpma@gmail.com>
2025-04-05 21:11:23 +0700

    config theme color

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import "./App.css";
-
 function App() {
-  return <div className="text-red-400">hello world</div>;
+  return (
+    <>
+      <div className="text-textNormal w-[100px] h-[100px] bg-bgLight">
+        Hello world
+      </div>
+      <div className="text-textLight w-[100px] h-[100px] bg-bgDark">
+        Hello world
+      </div>
+      <div className="text-textDark w-[100px] h-[100px] bg-bgNormal">
+        Hello world
+      </div>
+    </>
+  );
 }
 
 export default App;
