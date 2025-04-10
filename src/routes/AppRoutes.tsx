35bf29915ea24ff303cc60364535d7f88dc3bad5
hunh Liem <liemhuynh0789@gmail.com>
2025-04-10 17:28:08 +0700

    update route xem ket qua tung cv

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -13,6 +13,14 @@ const AppRoutes: React.FC = () => {
       <Route path="/" element={<Home />} />
       <Route path="/profile" element={<Profile />} />
       
+      <Route path="/details/:id" element={<DetailCV />}>
+        {/* Sub-routes for the tabs */}
+        <Route index element={<SkillsTab />} />
+        <Route path="skills" element={<SkillsTab />} />
+        <Route path="analysis" element={<AnalysisTab />} />
+        <Route path="suggestions" element={<SuggestionsTab />} />
+      </Route>
+
       {/* Route cha cho DetailCV */}
       <Route path="/details" element={<DetailCV />}>
         {/* Các route con sẽ tự động được render vào <Outlet /> trong DetailCV */}
