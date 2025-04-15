1becea909442b5501edcd13afdeba3821b04cbb3
trgcghai <conghai.tpma@gmail.com>
2025-04-15 21:27:56 +0700

    remove redundant export button

--- src/pages/AdminDashboard.tsx
+++ src/pages/AdminDashboard.tsx
@@ -3,30 +3,12 @@ import UserActivityChart from "../components/chart/UserActivityChart";
 import CVScorePieChart from "../components/chart/CvScorePieChart";
 import RecentUsersTable from "../components/table/RecentUsersTable";
 import RecentCVsTable from "../components/table/RecentCvsTable";
-import { Button } from "@/components/ui/button";
 
 const AdminDashboard = () => {
   return (
     <div className="space-y-6">
       <div className="flex items-center justify-between">
         <h2 className="text-2xl font-bold text-gray-800">Dashboard</h2>
-        <Button className="flex items-center text-textNormal gap-2 bg-white border text-sm font-medium px-4 py-2 rounded-md hover:bg-mainHover hover:text-white transition duration-200 ease-in-out">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="w-4 h-4"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M4 16v2a2 2 0 002 2h12a2 2 0 002-2v-2M7 10l5 5m0 0l5-5m-5 5V4"
-            />
-          </svg>
-          Xuất báo cáo
-        </Button>
       </div>
 
       <OverviewCards />
